72d500f88094b8f850f037c2d088769dffd0c694
Ziyaul Mustafa <Ziyaulmustafa.Jangi@in.bosch.com>
2025-07-28 20:20:27 +0530

    Used internal pull down register instead of external pull down register, code functions properly

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -277,7 +277,7 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin : User_Button_Pin */
   GPIO_InitStruct.Pin = GPIO_PIN_1;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pin : Led_Pin */
