663c65cabd218f077ece49b9ebff0a71f5699b59
Ziyaul Mustafa <Ziyaulmustafa.Jangi@in.bosch.com>
2025-08-08 15:31:22 +0530

    SPI communication with shift register 74hc165 - D0 and D1 are parallel inputs - Q7 is serial ouput - 8 bit random data is sent to shiftregister using SPI to proivde clock to shift register so that it can send data to nuclei. - Input Data is fed using switches and ouput is received using SPI on MISO(PA6) and printed via UART Expected ouput:  - D1   D0  E  - 0     0    0 -  0     1    1 -  1     0    2 -  1     1    3 Output:  - D1   D0  E  - 0     0    0 -  0     1    3 -  1     0    5 -  1     1    7 - some floating point issue with wokwi even though D2 to D7 are grounded.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -17,6 +17,7 @@
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
+#include<stdio.h>
 #include "main.h"
 #include "stm32c0xx_hal.h"
 #include "stm32c0xx_hal_spi.h"
@@ -43,9 +44,9 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-
+I2C_HandleTypeDef hi2c1;
 SPI_HandleTypeDef hspi1;
-
+UART_HandleTypeDef huart2;
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
@@ -54,6 +55,8 @@ SPI_HandleTypeDef hspi1;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_SPI1_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -92,14 +95,31 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
 
+  MX_USART2_UART_Init();
+  MX_I2C1_Init();
   MX_SPI1_Init();
+  
+  uint8_t txData[] = {0xAA};  // Any dummy byte
+  uint8_t rxData[1];
+  char msg[50];
 
-  uint8_t data[] = {0xA5, 0x5A, 0xFF};
-
+  HAL_UART_Transmit(&huart2, (uint8_t*)"Running...\r\n", 12, HAL_MAX_DELAY);
   while (1)
   {
-    HAL_SPI_Transmit(&hspi1, data, sizeof(data), HAL_MAX_DELAY);
-    HAL_Delay(100); 
+    // Latch data from 74HC165 inputs
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET); // SH/LD = LOW (load inputs)
+    HAL_Delay(1);
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);   // SH/LD = HIGH (shift mode)
+
+    // Send & receive one byte
+    if (HAL_SPI_TransmitReceive(&hspi1, txData, rxData, 1, HAL_MAX_DELAY) == HAL_OK)
+    {
+      // Use received byte from MISO
+      sprintf(msg, "Received byte: 0x%02X\r\n", rxData[0]);
+      HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
+    }
+
+    HAL_Delay(1000);
   }
 }
 
@@ -159,6 +179,98 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   GPIO_InitStruct.Alternate = GPIO_AF0_SPI1;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  // PA4 as output for shift register latch (SH/LD)
+  GPIO_InitStruct.Pin = GPIO_PIN_4;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  
+}
+
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.Timing = 0x20303E5D;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
 
 }
 
